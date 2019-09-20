20c1373f89951d536d0ad75d02a04f90d65382a1
YDUP <ydup@foxmail.com>
2019-09-20 21:23:59 +0800

    v1.0 for xgbt

--- main.py
+++ main.py
@@ -4,43 +4,46 @@ import datetime
 import pandas as pd
 import numpy as np
 import os
-import seaborn as sns
-import matplotlib.pyplot as plt
-sns.set(style="darkgrid")
-plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-plt.rcParams['axes.unicode_minus']=False #用来正常显示
-from zipfile import ZipFile
-import pickle
-# set the environment path to find Recommenders
 import sklearn
-import numpy as np 
-import pandas as pd
-import os
-import seaborn as sns
-color = sns.color_palette()
-import matplotlib.pyplot as plt
-from math import atan
 from sklearn import preprocessing
 from sklearn import decomposition
 from xgboost import plot_importance
 from tqdm import tqdm
-
 from sklearn.model_selection import ParameterGrid
-def rec2polar(x, y):
-    x, y = float(x), float(y)
-    R = np.sqrt(x**2 + y**2)  # the length from (tmp_x, tmp_y) to (0, 0)
-    if x == 0:
-        # the point is at y axis
-        theta = np.sign(y)*np.pi/2
-    elif x > 0:
-        # the point is at I and IV regions
-        theta = atan(y/x)
-    elif x < 0:
-        # the point is at II and III regions
-        theta = np.sign(y)*np.pi+atan(y/x)
-        if y == 0:
-            theta = -np.pi
-    return R, theta
+from sklearn.metrics import mean_absolute_error,mean_squared_error
+from xgboost import XGBRegressor
+from sklearn import metrics
+from sklearn.metrics import explained_variance_score
+import xgboost as xgb
+from sklearn.model_selection import GridSearchCV, KFold, cross_val_score
+from math import sqrt
+from sklearn.linear_model import ElasticNet, Lasso,  BayesianRidge, LassoLarsIC
+from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor, AdaBoostRegressor
+from sklearn.linear_model import Ridge
+import time
+
+def Invilidation(predictions, val_class):
+    pre_class=np.zeros(len(val_class))
+    for k in range(len(predictions)):
+        if predictions[k]<-103:
+            pre_class[k]=1.
+    TP=0
+    FP=0
+    FN=0
+    TN=0
+    for i in range(len(val_class)):
+        if val_class[i]==1. and pre_class[i]==val_class[i]:
+            TP=TP+1
+        if val_class[i]==0. and pre_class[i]==1.:
+            FP=FP+1
+        if val_class[i]==1. and pre_class[i]==0.:
+            FN=FN+1
+        if val_class[i]==0. and pre_class[i]==val_class[i]:
+            TN=TN+1
+    Recall=TP/(TP+FN)
+    Precision =TP/(TP+FP)
+    PCRR=2*Precision*Recall/(Precision+Recall)
+    return Recall,Precision,PCRR
 
 def rec2polar_array(x, y):
     R = np.sqrt(x**2 + y**2)  # the length from (tmp_x, tmp_y) to (0, 0)
@@ -67,9 +70,6 @@ def sliceXY(data):
     plane[plane==5] = 3
     data.loc[:, 'XYpower'] = plane
     return data
-
-def sliceZ(data):
-    pass
 
 def ConvHeight(data, kernel_r = 1):
     bins = (np.arange(data.loc[:, 'X_relative'].min()-10.0, data.loc[:, 'X_relative'].max()+10.0, 5.0), 
@@ -118,117 +118,87 @@ def add_clutter_type(data):
 
 def add_feature(data):
     '''Add new features into data'''
-    data.loc[:, 'Effective_Cell_Height'] = data.loc[:, 'Height'] + data.loc[:, 'Cell Altitude']
-    data.loc[:, 'Effective_Height'] = data.loc[:, 'Altitude'] + data.loc[:, 'Building Height']
-    data.loc[:, 'seperation_distance'] = np.sqrt((data.loc[:, 'X'] - data.loc[:, 'Cell X'] - 2.5)**2 + (data.loc[:, 'Y'] - data.loc[:, 'Cell Y'] + 2.5)**2)
-    data.loc[:, 'dh'] = np.log10(1+data.loc[:, 'seperation_distance'])*np.log10(1+data.loc[:, 'Effective_Cell_Height'])
-    data.loc[:, 'X_relative'] = data.loc[:, 'X'] - data.loc[:, 'Cell X'] - 2.5
-    data.loc[:, 'Y_relative'] = data.loc[:, 'Y'] - data.loc[:, 'Cell Y'] + 2.5
-    data.loc[:, 'deltaH'] = data.loc[:, 'Effective_Cell_Height'] - data.loc[:, 'Effective_Height'] - data.loc[:, 'seperation_distance']*np.tan(np.pi/180.0*(data.loc[:, 'Electrical Downtilt']+data.loc[:, 'Mechanical Downtilt']))
+    data.loc[:, 'Effective_Cell_Height'] = data.loc[:, 'Height'].values + data.loc[:, 'Cell Altitude'].values
+    data.loc[:, 'Effective_Height'] = data.loc[:, 'Altitude'].values + data.loc[:, 'Building Height'].values
+    data.loc[:, 'seperation_distance'] = np.sqrt((data.loc[:, 'X'].values - data.loc[:, 'Cell X'].values - 2.5)**2 + (data.loc[:, 'Y'].values - data.loc[:, 'Cell Y'].values + 2.5)**2)
+    data.loc[:, 'dh'] = np.log10(1+data.loc[:, 'seperation_distance'].values)*np.log10(1+data.loc[:, 'Effective_Cell_Height'].values)
+    data.loc[:, 'X_relative'] = data.loc[:, 'X'].values - data.loc[:, 'Cell X'].values - 2.5
+    data.loc[:, 'Y_relative'] = data.loc[:, 'Y'].values - data.loc[:, 'Cell Y'].values + 2.5
+    data.loc[:, 'deltaH'] = data.loc[:, 'Effective_Cell_Height'].values - data.loc[:, 'Effective_Height'].values - data.loc[:, 'seperation_distance'].values*np.tan(np.pi/180.0*(data.loc[:, 'Electrical Downtilt'].values + data.loc[:, 'Mechanical Downtilt'].values))
 
-    data.loc[:, 'distance_3D'] = np.sqrt(data.loc[:, 'seperation_distance']**2 + data.loc[:, 'deltaH']**2)
+    data.loc[:, 'distance_3D'] = np.sqrt(data.loc[:, 'seperation_distance'].values**2 + data.loc[:, 'deltaH'].values**2)
     data.loc[:, 'vertical_theta'] = rec2polar_array(data.loc[:, 'deltaH'].values, data.loc[:, 'seperation_distance'].values)*180.0/np.pi
     data.loc[:, 'horizon_theta'] = rec2polar_array(data.loc[:, 'X_relative'].values, data.loc[:, 'Y_relative'].values)
+    
     deltaTheta, theta_relative = [], []
-    thetaXY = 90 - data.loc[:, 'horizon_theta']*180.0/np.pi
+    thetaXY = 90 - data.loc[:, 'horizon_theta'].values*180.0/np.pi
     thetaXY[thetaXY<0] += 360
     data.loc[:, 'horizon_theta'] = thetaXY
-    deltaTh = thetaXY - data.loc[:, 'Azimuth']
+    deltaTh = thetaXY - data.loc[:, 'Azimuth'].values
     deltaTh[deltaTh<0] += 360
     data.loc[:, 'deltaTheta'] = deltaTh
-
-    tempdata = preprocessing.scale(data.drop('RSRP', axis=1).values, axis=0)
+    try:
+        tempdata = preprocessing.scale(data.drop('RSRP', axis=1).values, axis=0)
+    except: 
+        tempdata = preprocessing.scale(data.values, axis=0)
+        pass
     pca = decomposition.PCA(n_components=3)
     pcafeature = pca.fit_transform(tempdata)
-    for idx, name in enumerate(['pca{0}'.format(i) for i in range(3)]):
+    print(pcafeature.shape)
+    for idx in np.arange(3):
         # data.loc[:, ['pca{0}'.format(i) for i in range(3)]]=pcafeature
-        data.loc[:, name] = pcafeature[:, idx]
-    clutter_class = np.unique(data.loc[:, 'Clutter Index'].values)
+        # ['pca{0}'.format(i) for i in range(3)]
+        data.loc[:, 'pca_{0}'.format(idx)] = pcafeature[:, idx]
+    
+    clutter_class = np.arange(1, 21)
     clutter = []
     for cl in data.loc[:, 'Clutter Index'].values:
-        clutter.append(np.array(cl == np.array(clutter_class), dtype=np.int32))
+        clutter.append(np.array(cl == np.array(clutter_class), dtype=np.float32))
     clutter = np.array(clutter)
-    for idx, name in enumerate(['one_hot{0}'.format(i) for i in range(len(clutter_class))]):    
+    for idx, name in enumerate(['one_hot'+str(int(i)) for i in range(len(clutter_class))]):    
         # data.loc[:, ['pca{0}'.format(i) for i in range(3)]]=pcafeature
         data.loc[:, name] = clutter[:, idx]
+    
     # data = data.drop(['X', 'Y', 'Cell X', 'Cell Y', 'Clutter Index'], axis=1) 
     #data = ConvHeight(data, 1)
     #data = ConvHeight(data, 3)
     #data = ConvHeight(data, 5)
     data = add_clutter_type(data)
+    
     return data
 
-def get_data(data_path):
+
+def gen_data(data_path, test_size=5):
     list_names=os.listdir(data_path)
-    data=pd.read_csv(os.path.join(data_path,list_names[0]))
-    print(list_names[0])
-    data=add_feature(data)
-    for i in tqdm(range(1,500)):
-        one_data=pd.read_csv(os.path.join(data_path,list_names[i]))
-        one_data=add_feature(one_data)
-        data=pd.concat((data,one_data), axis=0)
-        #if i%100==0:
-        #    print(i)
-    return data
+    data=pd.read_csv(os.path.join(data_path, list_names[0]))
+    data = add_feature(data)
+    data.to_csv('train.csv', index=False)
+    list_names = list_names[0: 6]
+    for i in tqdm(range(1,  len(list_names))):
+        data=pd.read_csv(os.path.join(data_path,list_names[i]))
+        data=add_feature(data)
+        if i < (len(list_names) - test_size):
+            data.to_csv('train.csv', index=False, header=False, mode='a+')
+        elif i == (len(list_names) - test_size):
+            data.to_csv('test.csv', index=False)
+        else:
+            data.to_csv('test.csv', index=False, header=False, mode='a+')
 
-data=get_data('./train_set/')
+gen_data('../../Downloads/train_set/')
+train = pd.read_csv('train.csv')
+validate = pd.read_csv('test.csv')
 
-y=data['RSRP']
-#y=np.log1p(-y)
-X=data.drop('RSRP',axis=1)
-X=np.log1p(X)
+test = {'Name': [], 'Data': []}
+for testfile in os.listdir('../../Downloads/test_set/'):
+    test['Data'].append(np.log1p(add_feature(pd.read_csv(os.path.join('../../Downloads/test_set/',testfile)))))
+    test['Name'].append(testfile)
 
+trainX, trainY = train.drop('RSRP',axis=1), train['RSRP']
+trainX = np.log1p(trainX)
 
-#分割训练集和验证集
-from sklearn.model_selection import train_test_split
-train_X, val_X, train_y, val_y=train_test_split(X,y,test_size=0.1,random_state=1)
+valX, valY = validate.drop('RSRP',axis=1), validate['RSRP']
+valX = np.log1p(valX)
 
-from sklearn.metrics import mean_absolute_error,mean_squared_error
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from xgboost import XGBRegressor
-from sklearn import metrics
-from sklearn.metrics import explained_variance_score
-import xgboost as xgb
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import KFold, cross_val_score
-from math import sqrt
-from sklearn.linear_model import ElasticNet, Lasso,  BayesianRidge, LassoLarsIC
-from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor, AdaBoostRegressor
-from sklearn.linear_model import Ridge
-
-
-#lightgbm模型
-import time
-import lightgbm as lgb
-'''
-lgb_model = lgb.LGBMRegressor(objective='regression',num_leaves=7,
-                              learning_rate=0.05, n_estimators=200,
-                              max_bin = 50, bagging_fraction = 0.8,
-                              bagging_freq = 5, feature_fraction = 0.2,
-                              feature_fraction_seed=9, bagging_seed=9,
-                              min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
-begin_time=time.time()
-lgb_model.fit(X,y)
-print('消耗时间 %f s' % (time.time()-begin_time))
-
-#开始验证
-X_predictions = lgb_model.predict(val_X)
-
-#mae=mean_absolute_error(-np.expm1(X_predictions),-np.expm1(val_y.values))
-Mae=mean_absolute_error(X_predictions,val_y.values)
-
-#rmse=sqrt(mean_squared_error(-np.expm1(X_predictions),-np.expm1(val_y.values)))
-Rmse=sqrt(mean_squared_error(X_predictions,val_y.values))
-#输出验证mae,rmse
-#print("mae : %.4g" % mae)
-print("Mae : %.4g" % Mae)
-#print("rmse : %.4g" % rmse)
-print("Rmse : %.4g" % Rmse)
-'''
-
-
-# xgboost模型
 xgb_model = XGBRegressor(booster='gbtree',
                     objective= 'reg:linear',
                     eval_metric='rmse',
@@ -239,27 +209,35 @@ xgb_model = XGBRegressor(booster='gbtree',
                     colsample_bytree= 0.8,
                     tree_method= 'exact',
                     learning_rate=0.1,
-                    n_estimators=30,
+                    n_estimators=300,
                     nthread=4,
                     scale_pos_weight=1,
                     seed=27)
 
-# fit rf_model_on_full_data on all data from the training data
 begin_time=time.time()
 #score=cv_rmse(xgb_model,X,y)
-xgb_model.fit(X,y)
-print('消耗时间 %f s' % (time.time()-begin_time))
+xgb_model.fit(trainX, trainY)
+print('Fitting: %f s' % (time.time()-begin_time))
 
-#开始验证
-X_predictions = xgb_model.predict(val_X)
+valY_predict = xgb_model.predict(valX)
 # plot_importance()
-mae=mean_absolute_error(np.expm1(X_predictions),np.expm1(val_y.values))
-Mae=mean_absolute_error(X_predictions,val_y.values)
 
-rmse=sqrt(mean_squared_error(np.expm1(X_predictions),np.expm1(val_y.values)))
-Rmse=sqrt(mean_squared_error(X_predictions,val_y.values))
-#输出验证mae,rmse
-print("mae : %.4g" % mae)
-print("Mae : %.4g" % Mae)
-print("rmse : %.4g" % rmse)
+Rmse = sqrt(mean_squared_error(valY_predict,valY.values))
+val_class = np.zeros_like(valY)
+
+val_class[valY.values<-103] = 1 
+Recall, Precision, PCRR = Invilidation(valY_predict, val_class)
+
+print("Recall : %.4g" % Recall)
+print("Precision : %.4g" % Precision)
+print("PCRR : %.4g" % PCRR)
 print("Rmse : %.4g" % Rmse)
+ 
+for name, data in zip(test['Name'], test['Data']):
+    predict_test = xgb_model.predict(data)
+    dict_output = {'RSRP': None}
+    dict_output['RSRP'] = np.reshape(predict_test, [-1, 1]).tolist()
+    print(dict_output)
+    with open('{0}_result.txt'.format(name), "w") as f:
+        f.write(str(dict_output))
+
